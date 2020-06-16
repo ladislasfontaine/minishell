ae3bdc7c5456ae1594e205ab379c203e676181d2
ladislasfontaine <fontaine.ladislas@gmail.com>
2020-06-16 14:53:20 +0200

    minor change

--- sources/minishell.c
+++ sources/minishell.c
@@ -6,15 +6,20 @@
 /*   By: lafontai <lafontai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/15 11:41:09 by lafontai          #+#    #+#             */
-/*   Updated: 2020/06/16 10:52:29 by lafontai         ###   ########.fr       */
+/*   Updated: 2020/06/16 12:18:59 by lafontai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-	parser la commande pour remplacer les variables s'il y en a
-	est-ce que $(commande) est possible ?
-	gérer les \ + caractère spécial
-	redirections à gérer mais que vers un fichier avec une path (pas vers un fd)
+	make a main struct minishell to store all the data
+
+	1/ create a prompt (minishell$) and wait
+	2/ when enter is pressed get the command to parse
+	3/ routing toward the right function based on the command
+	4/ do the action (or error message)
+		- replace variables
+		- split commands if multiple with pipes
+	5/ come back to the prompt (unless it was exit)
 */
 
 int		main(void)

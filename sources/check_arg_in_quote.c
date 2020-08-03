4da174047cc5449c584824adfaa2c36c6e1e9eb9
krcdb <medo.mrt@gmail.com>
2020-08-03 14:19:32 +0200

    chevron in quote ok

--- sources/check_arg_in_quote.c
+++ sources/check_arg_in_quote.c
@@ -6,7 +6,7 @@
 /*   By: memartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/30 15:35:14 by memartin          #+#    #+#             */
-/*   Updated: 2020/07/31 13:47:01 by user42           ###   ########.fr       */
+/*   Updated: 2020/08/03 14:18:58 by memartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,29 +37,54 @@ static int	w_count(const char *str, char c)
 	return (d.count);
 }
 
-int			*check_arg_in_quote(t_command *cmd)
+static int	*create_tab(t_command *cmd)
 {
 	int		*tab;
 	int		i;
-	int		j;
 
 	i = 0;
-	j = 0;
 	cmd->nb_arg = w_count(cmd->cmd, ' ');
-	if (!(tab = (int*)malloc(sizeof(int) * (cmd->nb_arg + 2))))
+	if (!(tab = (int*)malloc(sizeof(int) * cmd->nb_arg + 1)))
 		return (NULL);
+	while (i < cmd->nb_arg)
+		tab[i++] = 0;
+	return (tab);
+}
+
+static int	*fill_tab(t_command *cmd, int *tab)
+{
+	t_split		d;
+	int			i;
+	int			j;
+
+	init_split(&d);
+	i = 0;
+	j = 0;
 	while (cmd->cmd[i])
 	{
 		while (cmd->cmd[i] && is_whitespace(cmd->cmd[i]))
 			i++;
-		if (cmd->cmd[i] != '\'' && cmd->cmd[i] != '\"')
-			tab[j] = 0;
-		else
+		if (cmd->cmd[i] == '\'' || cmd->cmd[i] == '\"')
+		{
 			tab[j] = 1;
+			check_quotes(cmd->cmd[i++], &d.s_quote, &d.d_quote);
+			while (cmd->cmd[i] && (d.s_quote || d.d_quote))
+				check_quotes(cmd->cmd[i++], &d.s_quote, &d.d_quote);
+		}
 		j++;
 		while (cmd->cmd[i] && !is_whitespace(cmd->cmd[i]))
 			i++;
 	}
-	tab[j] = 0;
+	return (tab);
+}
+
+int			*check_arg_in_quote(t_command *cmd)
+{
+	int		*tab;
+
+	tab = create_tab(cmd);
+	if (tab == NULL)
+		return (tab);
+	tab = fill_tab(cmd, tab);
 	return (tab);
 }
